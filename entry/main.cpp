be780318575aa06519bd92320e889bd8cc25c1c5
Eric Roth <ecrsoccerdude@gmail.com>
2022-11-11 02:12:51 +0000

    main.cpp driver

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <vector>
 
+#include "parse.h"
+#include "graph.h"
+#include "edge.h"
+
 /**
  * Driver file for the final project.
  * 
  * @todo command-line arguments for which .csv file to parse
  */
 int main() {
-    std::cout << "It works!" << std::endl;
+    /* Tester to check parsing */
+    std::string fname = "../data/test1.csv";
+    Parser* p = new Parser();
+    Graph* g = p->generateGraph(fname);
+    g->printGraph();
+
+    delete p;
+    delete g;
 
     return 0;
 }
